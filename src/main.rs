ba8c3a762d41fffb745cbb858eb2c54a3ac3684b
NewonetOrginal <jas.tryka@gmail.com>
2026-08-16 23:26:14 +0200

    Major change in struct structure

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,54 @@
-// audiofetch
-//
-// STYLE:
-// just like fast fetch but showing the data from the mpris about music
-//
-// USEFULL:
-// structs for data handling
 use mpris::PlayerFinder;
 use std::error::Error;
+use std::time::Duration;
 
-struct TrackInfo {
-    player: String,
+#[derive(Debug)]
+
+struct TrackInfo<'a> {
+    player: &'a str,
     playback_state: String,
-    title: String,
-    artist: Option<String>,
-    album: Option<String>,
-    art_url: Option<String>,
-    length_sec: Option<u64>,
+    title: &'a str,
+    artists: Vec<&'a str>,
+    album_name: &'a str,
+    art_url: Option<&'a str>,
+    length_sec: u64,
 }
 
-impl TrackInfo {
+impl<'a> TrackInfo<'a> {
     fn fmt_data(&self) -> String {
-        let player = format!("Player: {}", &self.player);
-        let playback_state = format!("Playback State: {}", &self.playback_state);
-        let title = format!("Title: {}", &self.title);
-        let artist = match &self.artist {
-            Some(art) => format!("Artist: {}", art),
-            None => String::from("Artist: Unknown"),
-        };
-        let album = match &self.album {
-            Some(album) => format!("Album: {}\n", album),
-            None => String::from(""),
-        };
-        let art_url = match &self.art_url {
-            Some(art) => format!("still to work on!!!"),
-            None => String::from(""),
-        };
-        let length = match self.length_sec {
-            Some(len) => {
-                let min = len / 60;
-                let sec = len % 60;
-                format!("length: {}:{}", min, sec)
-            }
-            None => String::from("length: Unknown"),
-        };
-        format!("{player}\n{playback_state}\n{title}\n{artist}\n{album}{length}")
+        format!("")
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let player_finder = PlayerFinder::new()?;
     let find_active = player_finder.find_active()?;
-    let playback_status = find_active.get_playback_status()?;
-
     let metadata = find_active.get_metadata()?;
-    let title = match metadata.title() {
-        Some(title) => format!("{title}"),
-        None => String::from(""),
+
+    let player = find_active.identity();
+    let playback_state = find_active.get_playback_status()?;
+    let Some(title) = metadata.title() else {
+        return Ok(());
+    };
+    let Some(artists) = metadata.artists() else {
+        return Ok(());
+    };
+    let Some(album_name) = metadata.album_name() else {
+        return Ok(());
+    };
+    let Some(length) = metadata.length() else {
+        return Ok(());
     };
 
-    let test_data = TrackInfo {
-        player: String::from(find_active.identity()),
-        playback_state: format!("{playback_status:?}"),
-        title: format!("{title}"),
-        artist: Some(String::from("Hysia")),
-        album: Some(String::from("Revive")),
-        // album: None,
-        art_url: Some(String::from("")),
-        length_sec: Some(302),
+    let trackinfo = TrackInfo {
+        player: player,
+        playback_state: format!("{playback_state:?}"),
+        title: title,
+        artists: artists,
+        album_name: album_name,
+        art_url: None,
+        length_sec: length.as_secs(),
     };
-    let output = test_data.fmt_data();
-
-    println!("{output}");
+    println!("{:#?}", trackinfo);
     Ok(())
 }
