c1a3f5b5c7c003a945e79bfcdfbd749c5bd971ed
NewonetOrginal <jas.tryka@gmail.com>
2026-09-25 22:48:00 +0200

    Made it work??? Now makes actually the ASNI art frommm the cover of the track

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use crossterm::style::Stylize;
+use crossterm::style::{Color, Stylize};
+use image::{
+    DynamicImage, GenericImageView, Pixel, imageops::FilterType::Lanczos3, load_from_memory,
+};
 use mpris::PlayerFinder;
+use reqwest::blocking::get;
 use std::error::Error;
+use std::fmt::Write;
+use std::fs;
 use std::iter::zip;
+use url::Url;
 
 struct TrackInfo<'a> {
     player: &'a str,
@@ -88,25 +95,98 @@ impl<'a> TrackInfo<'a> {
     }
 }
 
-fn ascii_art() -> Vec<String> {
-    let art = vec![
-        r#" ,_     _        "#.to_string(),
-        r#" |\\_,-~/        "#.to_string(),
-        r#" / _  _ |    ,--."#.to_string(),
-        r#"(  @  @ )   / ,-'"#.to_string(),
-        r#" \  _T_/-._( (   "#.to_string(),
-        r#" /         `. \  "#.to_string(),
-        r#"|         _  \ | "#.to_string(),
-        r#" \ \ ,  /      | "#.to_string(),
-        r#"  || |-_\__   /  "#.to_string(),
-        r#"((_/`(____,-'    "#.to_string(),
-    ];
-    art
+// fn ascii_art() -> Vec<String> {
+//     let art = vec![
+//         r#" ,_     _        "#.to_string(),
+//         r#" |\\_,-~/        "#.to_string(),
+//         r#" / _  _ |    ,--."#.to_string(),
+//         r#"(  @  @ )   / ,-'"#.to_string(),
+//         r#" \  _T_/-._( (   "#.to_string(),
+//         r#" /         `. \  "#.to_string(),
+//         r#"|         _  \ | "#.to_string(),
+//         r#" \ \ ,  /      | "#.to_string(),
+//         r#"  || |-_\__   /  "#.to_string(),
+//         r#"((_/`(____,-'    "#.to_string(),
+//     ];
+//     art
+// }
+
+fn parse_in(url: &str) -> Result<DynamicImage, Box<dyn std::error::Error>> {
+    let fallback = "https://i.scdn.co/image/ab67616d0000b273c8329930b500b5603a4482a2";
+    if let Ok(parsed_url) = Url::parse(&url) {
+        match parsed_url.scheme() {
+            "http" | "https" => {
+                let raw_bytes = get(url)?.bytes()?;
+                let img = load_from_memory(raw_bytes.as_ref())?;
+                Ok(img)
+            }
+            "file" => {
+                let url = Url::parse(&url)?;
+                let url = Url::to_file_path(&url).map_err(|_| "Error: Invalid Url")?;
+                let raw_bytes = fs::read(url)?;
+                let img = load_from_memory(raw_bytes.as_ref())?;
+                Ok(img)
+            }
+            _ => {
+                let raw_bytes = get(fallback)?.bytes()?;
+                let img = load_from_memory(raw_bytes.as_ref())?;
+                Ok(img)
+            }
+        }
+    } else {
+        let raw_bytes = get(fallback)?.bytes()?;
+        let img = load_from_memory(raw_bytes.as_ref())?;
+        Ok(img)
+    }
+}
+fn color(img: &DynamicImage, x: u32, y: u32) -> [Color; 2] {
+    let pixelu_col = img.get_pixel(x, y);
+    // let pixell_col = img.get_pixel(x, y + 1);
+
+    let r: u8 = pixelu_col.channels()[0];
+    let g: u8 = pixelu_col.channels()[1];
+    let b: u8 = pixelu_col.channels()[2];
+
+    let color_up = Color::Rgb { r: r, g: g, b: b };
+
+    if y + 1 <= img.dimensions().1 - 1 {
+        let pixell_col = img.get_pixel(x, y + 1);
+        let r: u8 = pixell_col.channels()[0];
+        let g: u8 = pixell_col.channels()[1];
+        let b: u8 = pixell_col.channels()[2];
+
+        let color_down = Color::Rgb { r: r, g: g, b: b };
+
+        let color = [color_up, color_down];
+
+        color
+    } else {
+        let color = [color_up, color_up];
+
+        color
+    }
 }
 
-fn generate_block_buffer(width_cells: usize, height_cells: usize) -> Vec<String> {
-    let row = "\u{2588}".repeat(width_cells);
-    vec![row; height_cells]
+// fn generate_block_buffer(width_cells: usize, height_cells: usize) -> Vec<String> {
+//     let row = "\u{2588}".repeat(width_cells);
+//     vec![row; height_cells]
+// }
+
+fn art(img: &DynamicImage) -> Vec<String> {
+    let dimesions = img.dimensions();
+    let mut ascii_art: Vec<String> = Vec::with_capacity(46);
+
+    let ascii_char = '\u{2580}';
+
+    for y in (0..dimesions.1).step_by(2) {
+        let mut line = String::with_capacity(2000);
+        for x in 0..dimesions.0 {
+            let color = color(&img, x, y);
+            write!(line, "{}", ascii_char.with(color[0]).on(color[1])).unwrap();
+        }
+        ascii_art.push(line);
+    }
+    ascii_art
 }
 
 fn output(mut art: Vec<String>, mut info: Vec<String>) {
@@ -155,16 +235,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         };
 
         let info = trackinfo.fmt_data();
-        let art = ascii_art();
-        let buffer = generate_block_buffer(40, 20);
+        let size = (46, 46);
+        let img = parse_in(trackinfo.art_url)?.resize(size.0, size.1, Lanczos3);
 
-        output(art, info);
+        let ascii_art = art(&img);
+
+        // let buffer = generate_block_buffer(40, 20);
+
+        output(ascii_art, info);
 
         Ok(())
     } else {
-        let info = vec!["No Player ;(".bold().dark_red().to_string()];
-        let art = ascii_art();
-        output(art, info);
+        // TO DO
+
+        // let info = vec!["No Player ;(".bold().dark_red().to_string()];
+        // let ascii_art = art(trackinfo.art_url);
+
+        // output(ascii_art, info);
+
         Ok(())
     }
 }
